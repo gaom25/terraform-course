8d439bc62a9cb284e5490517b78b754ad972c28e
Gustavo Ortega <1707601+gaom25@users.noreply.github.com>
2021-07-29 16:34:04 -0500

    change region and jenkins version

--- jenkins-packer-demo/vars.tf
+++ jenkins-packer-demo/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -15,7 +15,7 @@ variable "INSTANCE_DEVICE_NAME" {
 }
 
 variable "JENKINS_VERSION" {
-  default = "2.204.5"
+  default = "2.289.3"
 }
 
 variable "TERRAFORM_VERSION" {
